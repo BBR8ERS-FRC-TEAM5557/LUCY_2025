7cc0e2cd1063421b8078ed622c86c11341944e86
Anya Krislav <97042471+akris011@users.noreply.github.com>
2025-02-27 15:57:06 -0500

    autos and L4 wrist adjustment

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -213,6 +213,7 @@ public class RobotContainer {
 
                 m_operator.povDown().whileTrue(SuperstructureFactory.scoreL3Coral());
                 m_operator.povDown().onFalse(SuperstructureFactory.stow());
+
                 m_operator.povLeft().whileTrue(SuperstructureFactory.scoreL4Coral());
                 m_operator.povLeft().onFalse(SuperstructureFactory.stow());
 
@@ -266,7 +267,13 @@ public class RobotContainer {
 
                 m_autoChooser.addDefaultOption("Do Nothing", null);
 
-                m_autoChooser.addDefaultOption("Drive_Back", AutoBuilder.buildAuto("Drive_Back"));
+                m_autoChooser.addDefaultOption("drive_back_left", AutoBuilder.buildAuto("drive_back_left"));
+
+                m_autoChooser.addDefaultOption("just_paths", AutoBuilder.buildAuto("just_paths")); //drive_back_right
+
+                m_autoChooser.addDefaultOption("drive_back_right", AutoBuilder.buildAuto("drive_back_right")); //drive_back_right
+
+                m_autoChooser.addDefaultOption("drive_back_right", AutoBuilder.buildAuto("drive_back_right")); //drive_back_right
 
                 // // Set up feedforward characterization
                 // m_autoChooser.addOption(
